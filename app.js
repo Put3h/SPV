8c5f937163b44de03bd76f52cc0eeb65340d2113
Mas Cool <tetehdanu@gmail.com>
2025-11-17 20:43:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let editIndex = -1;
 
 // Load Excel dari direktori web
 function loadExcel() {
-  fetch("keuangan.xlsx")
+  fetch("Keuangan.xlsx")
     .then(res => res.arrayBuffer())
     .then(buffer => {
       const workbook = XLSX.read(buffer, { type: "array" });
