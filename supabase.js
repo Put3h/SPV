ad5d4e00a0257f2f5f72f6098c20bdadb6d5364e
Mas Cool <tetehdanu@gmail.com>
2025-11-25 20:15:31 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,4 @@
 <script type="module">
   import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-  export const supabase = createClient("YOUR_URL","YOUR_ANON_KEY");
+  export const supabase = createClient("https://lahblqaaqadcmdowdsvd.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhaGJscWFhcWFkY21kb3dkc3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MDg5NzUsImV4cCI6MjA3OTE4NDk3NX0.MfRcythiqYSV-qfNMA7a8YRFDGsDmsn0JKbf1rQg_vM");
 </script>
-
