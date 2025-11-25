0574ea40ea0f7e3cd87f436ba83e2f7d02f81767
Mas Cool <tetehdanu@gmail.com>
2025-11-25 19:36:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // === MASUKKAN DATA SUPABASE DI SINI ===
 const supabase = createClient(
-  "https://lahblqaaqadcmdowdsvd.supabase.co",        // contoh: https://abcxyz.supabase.co
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhaGJscWFhcWFkY21kb3dkc3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MDg5NzUsImV4cCI6MjA3OTE4NDk3NX0.MfRcythiqYSV-qfNMA7a8YRFDGsDmsn0JKbf1rQg_vM"    // anon public key
+  "https://lahblqaaqadcmdowdsvd.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhaGJscWFhcWFkY21kb3dkc3ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2MDg5NzUsImV4cCI6MjA3OTE4NDk3NX0.MfRcythiqYSV-qfNMA7a8YRFDGsDmsn0JKbf1rQg_vM"
 );
 
 // === LOAD DATA ===
